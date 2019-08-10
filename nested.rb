b6b58cbedfc9809891b2449558969179ca9d5c61
Mark Rego <rego121@gmail.com>
2019-08-10 01:04:20 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-     alans_new_info = "GUI"
+programmer_hash[:dennis_ritchie][:languages]["C"] = "GUI"
+		 alans_new_info = "GUI"
 
 
 end
